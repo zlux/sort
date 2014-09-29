e82cb6dbfc8da4d1099f2d67ad0b0cb71d8c8b73
Julian <julian@julian-VirtualBox.(none)>
2014-09-30 00:25:58 +0800

    be aware of scanf

---
+++ bubble.c
@@ -0,0 +1,46 @@
+#include "stdio.h"
+
+void swap(int* pArray, int i, int j)
+{
+	int iTemp = pArray[i];
+	pArray[i] = pArray[j];
+	pArray[j] = iTemp;
+}
+void bubbleSort(int* pArray, int iLength)
+{
+	int i = 0;
+	int j = iLength -1;
+	
+	for(i = 0; i<iLength; i++)
+	{
+		j = iLength -1;
+		while(j > i)
+		{
+			if(pArray[j] < pArray[i])
+			swap(pArray, i, j);
+			j--;
+		}
+	}
+}
+int main(void)
+{
+	int buf[100] = {0};
+	int iLength = 0;
+	int i = 0;
+
+	while(scanf("%d", buf+iLength))
+	{
+		iLength ++;
+	}
+	
+	for(i = 0; i<iLength; i++)
+		printf("%d\t", buf[i]);
+	printf("\n");
+
+	bubbleSort(buf, iLength);
+
+	for(i = 0; i< iLength; i++)
+		printf("%d\t", buf[i]);
+	puts("\n");
+	getchar();	
+}
